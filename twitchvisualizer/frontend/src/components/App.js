a9ad8f53b573a976296cc416ccfdf9b901dbe7e2
Kevin <imbesci@gmail.com>
2022-04-15 13:00:33 -0400

    finihs setting up react with django

---
+++ twitchvisualizer/frontend/src/components/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { useEffect, useState, useRef } from 'react';
+import reactDOM from 'react-dom/client';
+
+
+export default function App(props){
+
+    return (
+        <>
+        <h1>TESTING REACT CODE WITH DJANGO</h1>
+        <button>TEST BUTTON</button>
+        </>
+    )
+}
+
+root = reactDOM.createRoot(document.getElementById('root'))
+root.render(<App />)
--- twitchvisualizer/twitchvisualizer/settings.py
+++ twitchvisualizer/twitchvisualizer/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
-
+import os 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'twitchvisualizer.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['./templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -117,7 +117,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
